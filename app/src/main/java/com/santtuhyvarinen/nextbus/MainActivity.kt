aba8e4a91b29c38c9fd004b3d4fe46a7020d2945
Santtu Hyvärinen <santtu.hyvarine@gmail.com>
2020-05-11 10:40:31 +0300

    Location permission

--- app/src/main/java/com/santtuhyvarinen/nextbus/MainActivity.kt
+++ app/src/main/java/com/santtuhyvarinen/nextbus/MainActivity.kt
@@ -1,29 +1,56 @@
 package com.santtuhyvarinen.nextbus
 
-import android.location.LocationManager
-import android.location.LocationProvider
+import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.santtuhyvarinen.nextbus.utils.PermissionUtil
 
 class MainActivity : AppCompatActivity() {
+
+    //All the permissions that app requires
+    val PERMISSIONS = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+
+    //Request code for permissions
+    val PERMISSION_ALL = 123
+
+    var permissionsGranted = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Navigation
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
         val navController = findNavController(R.id.nav_host_fragment)
-
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        //Permission - Ask user for permissions if they are not granted
+        if(!PermissionUtil.hasPermissions(this, *PERMISSIONS)) {
+            Log.d(PermissionUtil.LOG_PERMISSION, "Permissions not granted - Ask for permissions")
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        } else {
+            Log.d(PermissionUtil.LOG_PERMISSION, "All permissions granted")
+            permissionsGranted = true
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        permissionsGranted = PermissionUtil.hasPermissions(this, *PERMISSIONS)
+        Log.d(PermissionUtil.LOG_PERMISSION, "Permissions granted? " + permissionsGranted)
     }
 }
---
+++ app/src/main/java/com/santtuhyvarinen/nextbus/utils/PermissionUtil.kt
@@ -0,0 +1,19 @@
+package com.santtuhyvarinen.nextbus.utils
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+//Handles permissions - For now just the location permission
+class PermissionUtil {
+    companion object {
+        //For logging
+        const val LOG_PERMISSION = "permission_log"
+
+        fun hasPermissions(context: Context, vararg permissions: String): Boolean = permissions.all {
+            ActivityCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+}
